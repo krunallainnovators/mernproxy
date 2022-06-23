d5db2e714cf5759042712b839b38a4a15c4ddc12
krunalkanojiya12 <k_krunal@lainnovatorslab.com>
2022-06-23 11:31:41 +0530

    Delete

--- server.js
+++ server.js
@@ -13,12 +13,4 @@ app.get("/api/ping", (req, res) => {
   res.send("pong");
 });
 
-if (process.env.NODE_ENV == "production") {
-  app.use(express.static("client/build"));
-  const path = require("path");
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
-
 app.listen(port);
