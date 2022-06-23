c4a85e184b83ebd66d88eee927fe30cd7f65c408
krunalkanojiya12 <k_krunal@lainnovatorslab.com>
2022-06-23 11:59:10 +0530

    Client ed

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require("path");
 const app = express();
 const port = process.env.PORT || 4000;
 
-const DIST_DIR = path.join(__dirname, "build/static");
+const DIST_DIR = path.join(__dirname, "client/build/static");
 const HTML_FILE = path.join(DIST_DIR, "index.html");
 app.use(express.static(DIST_DIR));
 app.get("/", (req, res) => {
